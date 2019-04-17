43f9ee2b4e43331fc219eb31ce83279a64d4169e
AllaVved <46631193+AllaVved@users.noreply.github.com>
2019-04-17 15:02:30 +0300

    дубль № N

--- shop_toy/script/script.js
+++ shop_toy/script/script.js
@@ -1,29 +1,34 @@
-var button = document.getElementsByTagName("button");
 var getPrice = function () {
     for (let i = 0; i < product.length; i++) {
+        let button = document.getElementsByTagName("button");
         button[i].innerHTML = "Купить за " + product[i].price;
     }
-}
+};
 window.onload = getPrice;
+window.addEventListener('click', megaBasket);
+var summBasket = document.getElementById("summ");
+var summ = 0;
 
-function addAmount() {
-    let i = event.target.id;
+function megaBasket(evt) {
+    if (evt.target.classList.contains('addBasket') === true) {
+        addAmount(evt);
+    }
+};
+
+function addAmount(evt) {
+    let i = evt.target.value;
     product[i].amount++;
     getSumm(i);
-}
-
-function get() {
-    button.onclick = addAmount();
-}
-var summ = 0;
+    addToBasket(i);
+};
 
 function getSumm(i) {
     summ = summ + product[i].amount * product[i].price;
-    outputSumm();
-}
+    summBasket.innerText = "Сумма покупки = " + summ;
+};
 
-function outputSumm() {
-    let summBasket = document.getElementById("summ");
-    summBasket.innerHTML = "Сумма покупки = " + summ;
-}
-window.onclick = get;
+function addToBasket(i) {
+    if (product[i].amount === 1) {
+        document.querySelector('#basket').innerHTML += '<p>' + product[i].name + ' по цене ' + product[i].price /* + ' в количестве ' + product[i].amount*/ + '</p>';
+    } // else if (product[i].amount > 1){изменить элемент так чтобы в нем менялось product[i].amount }
+};
